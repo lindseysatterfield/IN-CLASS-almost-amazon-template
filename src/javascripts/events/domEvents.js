b45ae0540903649856ef94c7bd1995d7e0e01eb0
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-02-28 15:46:29 -0600

    added several features

--- src/javascripts/events/domEvents.js
+++ src/javascripts/events/domEvents.js
@@ -1,4 +1,7 @@
+import { showBooks } from '../components/books';
 import addBookForm from '../components/forms/addBookForm';
+import { createBook } from '../helpers/data/bookData';
+import addAuthorForm from '../components/forms/addAuthorForm';
 
 const domEvents = () => {
   document.querySelector('body').addEventListener('click', (e) => {
@@ -18,6 +21,16 @@ const domEvents = () => {
     // CLICK EVENT FOR SUBMITTING FORM FOR ADDING A BOOK
     if (e.target.id.includes('submit-book')) {
       console.warn('CLICKED SUBMIT BOOK', e.target.id);
+      e.preventDefault();
+      const bookObject = {
+        title: document.querySelector('#title').value,
+        image: document.querySelector('#image').value,
+        price: document.querySelector('#price').value,
+        sale: document.querySelector('#sale').checked,
+        author_id: document.querySelector('#author').value
+      };
+
+      createBook(bookObject).then((booksArray) => showBooks(booksArray));
     }
 
     // CLICK EVENT FOR SHOWING MODAL FORM FOR ADDING A BOOK
@@ -32,6 +45,10 @@ const domEvents = () => {
 
     // ADD CLICK EVENT FOR DELETING AN AUTHOR
     // ADD CLICK EVENT FOR SHOWING FORM FOR ADDING AN AUTHOR
+    if (e.target.id.includes('add-author-btn')) {
+      console.warn('CLICKED ADD AUTHOR BUTTON', e.target.id);
+      addAuthorForm();
+    }
     // ADD CLICK EVENT FOR SUBMITTING FORM FOR ADDING AN AUTHOR
     // ADD CLICK EVENT FOR EDITING AN AUTHOR
   });
--- src/javascripts/helpers/data/bookData.js
+++ src/javascripts/helpers/data/bookData.js
@@ -1,7 +1,36 @@
+import axios from 'axios';
+import firebaseConfig from '../auth/apiKeys';
 // API CALLS FOR BOOKS
 
+const dbUrl = firebaseConfig.databaseURL;
+
 // GET BOOKS
+const getBooks = () => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/books.json`)
+    .then((response) => resolve(Object.values(response.data)))
+    .catch((error) => reject(error));
+});
 // DELETE BOOK
+const deleteBook = (firebaseKey) => new Promise((resolve, reject) => {
+  axios.delete(`${dbUrl}/books/${firebaseKey}.json`)
+    .then(() => getBooks().then((booksArray) => resolve(booksArray)))
+    .catch((error) => reject(error));
+});
+
 // CREATE BOOK
+const createBook = (bookObject) => new Promise((resolve, reject) => {
+  axios.post(`${dbUrl}/books.json`, bookObject)
+    .then((response) => {
+      const body = { firebaseKey: response.data.name };
+      axios.patch(`${dbUrl}/books/${response.data.name}.json`, body)
+        .then(() => {
+          getBooks().then((booksArray) => resolve(booksArray));
+        });
+      console.warn(response.data.name);
+    }).catch((error) => reject(error));
+});
+
 // UPDATE BOOK
 // SEARCH BOOKS
+
+export { getBooks, createBook, deleteBook };
